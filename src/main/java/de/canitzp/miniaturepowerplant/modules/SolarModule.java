153a97b6c5952377e96c31ffcd311129315e07ba
canitzp <canitzp@gmail.com>
2022-01-05 16:11:23 +0100

    Plant Eater and Wind module

--- src/main/java/de/canitzp/miniaturepowerplant/modules/SolarModule.java
+++ src/main/java/de/canitzp/miniaturepowerplant/modules/SolarModule.java
@@ -4,13 +4,20 @@ import de.canitzp.miniaturepowerplant.carrier.TileCarrier;
 import de.canitzp.miniaturepowerplant.reasons.EnergyPenalty;
 import de.canitzp.miniaturepowerplant.reasons.EnergyProduction;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SolarModule extends DepletableItemModule {
 
@@ -29,6 +36,11 @@ public class SolarModule extends DepletableItemModule {
     @Override
     public void tick(Level world, BlockPos pos, TileCarrier tile, SynchroniseModuleData data) {
         if(!world.isClientSide()){
+            BlockPos startPos = pos.mutable().relative(Direction.DOWN, 10).relative(Direction.SOUTH, 10).relative(Direction.EAST, 10);
+            BlockPos endPos = pos.mutable().relative(Direction.UP, 10).relative(Direction.NORTH, 10).relative(Direction.WEST, 10);
+            List<BlockPos> collect = BlockPos.betweenClosedStream(startPos, endPos).filter(blockPos -> world.getBlockState(blockPos).getBlock().equals(Blocks.GRASS_BLOCK)).toList();
+            
+            collect.forEach(blockPos -> world.setBlock(blockPos, Blocks.DIRT.defaultBlockState(), 2));
             // energy production
             // from sunlight
             int calculateEnergy = SolarModule.calculateEnergy(world, pos);
