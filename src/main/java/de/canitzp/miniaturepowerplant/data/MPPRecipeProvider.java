dcb1b5e08bfc1f7ffef61444491dca7dac4cea93
canitzp <canitzp@gmail.com>
2023-03-15 15:13:09 +0100

    Better recipe unlocking and more usage of tags (mainly around copper), also added recipe for efficiency module

--- src/main/java/de/canitzp/miniaturepowerplant/data/MPPRecipeProvider.java
+++ src/main/java/de/canitzp/miniaturepowerplant/data/MPPRecipeProvider.java
@@ -7,6 +7,8 @@ import de.canitzp.miniaturepowerplant.modules.TemperatureModule;
 import de.canitzp.miniaturepowerplant.modules.WaterModule;
 import de.canitzp.miniaturepowerplant.modules.WindModule;
 import de.canitzp.miniaturepowerplant.upgrades.EcoUpgrade;
+import de.canitzp.miniaturepowerplant.upgrades.EfficiencyUpgrade;
+import net.minecraft.advancements.critereon.PlayerTrigger;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeCategory;
@@ -18,74 +20,72 @@ import net.minecraftforge.common.Tags;
 
 import java.util.function.Consumer;
 
-public class MPPRecipeProvider extends RecipeProvider{
-    
-    public MPPRecipeProvider(DataGenerator generator){
+public class MPPRecipeProvider extends RecipeProvider {
+
+    public MPPRecipeProvider(DataGenerator generator) {
         super(generator.getPackOutput());
     }
 
-    /*protected static InventoryChangeTrigger.TriggerInstance has(TagKey<Item> p_206407_) {
-        return inventoryTrigger(ItemPredicate.Builder.item().of(p_206407_).build());
-    }*/
-
     @Override
-    protected void buildRecipes(Consumer<FinishedRecipe> consumer){
+    protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.MISC, BlockCarrier.INSTANCE_ITEM)
-            .define('i', Tags.Items.INGOTS_IRON)
-            .define('b', Items.IRON_BARS)
-            .define('a', AccumulatorItem.ACCUMULATOR_BASIC)
-            .pattern("bib")
-            .pattern("aba")
-            .pattern("bib")
-            .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-            .save(consumer);
-        
+                .shaped(RecipeCategory.MISC, BlockCarrier.INSTANCE_ITEM)
+                .define('i', Tags.Items.INGOTS_IRON)
+                .define('b', Items.IRON_BARS)
+                .define('a', AccumulatorItem.ACCUMULATOR_BASIC)
+                .pattern("bib")
+                .pattern("aba")
+                .pattern("bib")
+                .unlockedBy("unlock_right_away", PlayerTrigger.TriggerInstance.tick())
+                .showNotification(false)
+                .save(consumer);
+
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.TOOLS, AccumulatorItem.ACCUMULATOR_BASIC)
-            .define('c', Items.COPPER_INGOT)
-            .define('r', Tags.Items.STORAGE_BLOCKS_REDSTONE)
-            .define('b', Items.COPPER_BLOCK)
-            .pattern(" b ")
-            .pattern("crc")
-            .pattern("crc")
-            .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-            .save(consumer);
-    
+                .shaped(RecipeCategory.TOOLS, AccumulatorItem.ACCUMULATOR_BASIC)
+                .define('c', Tags.Items.INGOTS_COPPER)
+                .define('r', Tags.Items.STORAGE_BLOCKS_REDSTONE)
+                .define('b', Items.COPPER_BLOCK)
+                .pattern(" b ")
+                .pattern("crc")
+                .pattern("crc")
+                .unlockedBy("unlock_right_away", PlayerTrigger.TriggerInstance.tick())
+                .showNotification(false)
+                .save(consumer);
+
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.TOOLS, AccumulatorItem.ACCUMULATOR_PLUS)
-            .define('c', Items.COPPER_INGOT)
-            .define('a', AccumulatorItem.ACCUMULATOR_BASIC)
-            .pattern("cac")
-            .pattern("cac")
-            .pattern("cac")
-            .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-            .save(consumer);
-    
+                .shaped(RecipeCategory.TOOLS, AccumulatorItem.ACCUMULATOR_PLUS)
+                .define('c', Tags.Items.INGOTS_COPPER)
+                .define('a', AccumulatorItem.ACCUMULATOR_BASIC)
+                .pattern("cac")
+                .pattern("cac")
+                .pattern("cac")
+                .unlockedBy("has_accumulator", has(AccumulatorItem.ACCUMULATOR_BASIC))
+                .save(consumer);
+
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.TOOLS, AccumulatorItem.ACCUMULATOR_ENHANCED)
-            .define('c', Items.COPPER_INGOT)
-            .define('a', AccumulatorItem.ACCUMULATOR_BASIC)
-            .define('b', AccumulatorItem.ACCUMULATOR_PLUS)
-            .pattern("cac")
-            .pattern("cbc")
-            .pattern("cac")
-            .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-            .save(consumer);
-        
+                .shaped(RecipeCategory.TOOLS, AccumulatorItem.ACCUMULATOR_ENHANCED)
+                .define('c', Tags.Items.INGOTS_COPPER)
+                .define('a', AccumulatorItem.ACCUMULATOR_BASIC)
+                .define('b', AccumulatorItem.ACCUMULATOR_PLUS)
+                .pattern("cac")
+                .pattern("cbc")
+                .pattern("cac")
+                .unlockedBy("has_accumulator_plus", has(AccumulatorItem.ACCUMULATOR_PLUS))
+                .save(consumer);
+
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.MISC, SolarModule.SOLAR_MODULE_WOOD)
-            .define('d', Items.DAYLIGHT_DETECTOR)
-            .define('l', Tags.Items.GEMS_LAPIS)
-            .define('q', Tags.Items.GEMS_QUARTZ)
-            .define('r', Tags.Items.DUSTS_REDSTONE)
-            .define('x', ItemTags.PLANKS)
-            .define('c', Items.COPPER_BLOCK)
-            .pattern("ldl")
-            .pattern("qrq")
-            .pattern("xcx")
-            .unlockedBy("has_copper_block", has(Items.COPPER_BLOCK))
-            .save(consumer);
+                .shaped(RecipeCategory.MISC, SolarModule.SOLAR_MODULE_WOOD)
+                .define('d', Items.DAYLIGHT_DETECTOR)
+                .define('l', Tags.Items.GEMS_LAPIS)
+                .define('q', Tags.Items.GEMS_QUARTZ)
+                .define('r', Tags.Items.DUSTS_REDSTONE)
+                .define('x', ItemTags.PLANKS)
+                .define('c', Tags.Items.STORAGE_BLOCKS_COPPER)
+                .pattern("ldl")
+                .pattern("qrq")
+                .pattern("xcx")
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
+                .save(consumer);
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.MISC, SolarModule.SOLAR_MODULE_STONE)
                 .define('d', Items.DAYLIGHT_DETECTOR)
@@ -93,11 +93,11 @@ public class MPPRecipeProvider extends RecipeProvider{
                 .define('q', Tags.Items.GEMS_QUARTZ)
                 .define('r', Tags.Items.STORAGE_BLOCKS_REDSTONE)
                 .define('x', Tags.Items.STONE)
-                .define('c', Items.COPPER_BLOCK)
+                .define('c', Tags.Items.STORAGE_BLOCKS_COPPER)
                 .pattern("ldl")
                 .pattern("qrq")
                 .pattern("xcx")
-                .unlockedBy("has_copper_block", has(Items.COPPER_BLOCK))
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
                 .save(consumer);
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.MISC, SolarModule.SOLAR_MODULE_IRON)
@@ -106,11 +106,11 @@ public class MPPRecipeProvider extends RecipeProvider{
                 .define('q', Tags.Items.GEMS_QUARTZ)
                 .define('r', Tags.Items.STORAGE_BLOCKS_REDSTONE)
                 .define('x', Tags.Items.INGOTS_IRON)
-                .define('c', Items.COPPER_BLOCK)
+                .define('c', Tags.Items.STORAGE_BLOCKS_COPPER)
                 .pattern("ldl")
                 .pattern("qrq")
                 .pattern("xcx")
-                .unlockedBy("has_copper_block", has(Items.COPPER_BLOCK))
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
                 .save(consumer);
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.MISC, SolarModule.SOLAR_MODULE_GOLD)
@@ -119,11 +119,11 @@ public class MPPRecipeProvider extends RecipeProvider{
                 .define('q', Tags.Items.GEMS_QUARTZ)
                 .define('r', Tags.Items.STORAGE_BLOCKS_REDSTONE)
                 .define('x', Tags.Items.INGOTS_GOLD)
-                .define('c', Items.COPPER_BLOCK)
+                .define('c', Tags.Items.STORAGE_BLOCKS_COPPER)
                 .pattern("ldl")
                 .pattern("qrq")
                 .pattern("xcx")
-                .unlockedBy("has_copper_block", has(Items.COPPER_BLOCK))
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
                 .save(consumer);
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.MISC, SolarModule.SOLAR_MODULE_LAPIS)
@@ -132,11 +132,11 @@ public class MPPRecipeProvider extends RecipeProvider{
                 .define('q', Tags.Items.GEMS_QUARTZ)
                 .define('r', Tags.Items.STORAGE_BLOCKS_REDSTONE)
                 .define('x', Tags.Items.STORAGE_BLOCKS_LAPIS)
-                .define('c', Items.COPPER_BLOCK)
+                .define('c', Tags.Items.STORAGE_BLOCKS_COPPER)
                 .pattern("ldl")
                 .pattern("qrq")
                 .pattern("xcx")
-                .unlockedBy("has_copper_block", has(Items.COPPER_BLOCK))
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
                 .save(consumer);
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.MISC, SolarModule.SOLAR_MODULE_REDSTONE)
@@ -145,11 +145,11 @@ public class MPPRecipeProvider extends RecipeProvider{
                 .define('q', Tags.Items.GEMS_QUARTZ)
                 .define('r', Tags.Items.STORAGE_BLOCKS_REDSTONE)
                 .define('x', Tags.Items.STORAGE_BLOCKS_REDSTONE)
-                .define('c', Items.COPPER_BLOCK)
+                .define('c', Tags.Items.STORAGE_BLOCKS_COPPER)
                 .pattern("ldl")
                 .pattern("qrq")
                 .pattern("xcx")
-                .unlockedBy("has_copper_block", has(Items.COPPER_BLOCK))
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
                 .save(consumer);
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.MISC, SolarModule.SOLAR_MODULE_DIAMOND)
@@ -157,11 +157,11 @@ public class MPPRecipeProvider extends RecipeProvider{
                 .define('d', Tags.Items.GEMS_DIAMOND)
                 .define('q', Tags.Items.GEMS_QUARTZ)
                 .define('r', Tags.Items.STORAGE_BLOCKS_REDSTONE)
-                .define('c', Items.COPPER_BLOCK)
+                .define('c', Tags.Items.STORAGE_BLOCKS_COPPER)
                 .pattern("sds")
                 .pattern("qrq")
                 .pattern("dcd")
-                .unlockedBy("has_copper_block", has(Items.COPPER_BLOCK))
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
                 .save(consumer);
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.MISC, SolarModule.SOLAR_MODULE_NETHERITE)
@@ -169,72 +169,84 @@ public class MPPRecipeProvider extends RecipeProvider{
                 .define('n', Tags.Items.INGOTS_NETHERITE)
                 .define('q', Tags.Items.GEMS_QUARTZ)
                 .define('r', Tags.Items.STORAGE_BLOCKS_REDSTONE)
-                .define('c', Items.COPPER_BLOCK)
+                .define('c', Tags.Items.STORAGE_BLOCKS_COPPER)
                 .pattern("sqs")
                 .pattern("qrq")
                 .pattern("ncn")
-                .unlockedBy("has_copper_block", has(Items.COPPER_BLOCK))
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
                 .save(consumer);
-        
+
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.MISC, TemperatureModule.TEMP_MODULE_BASIC)
-            .define('i', Tags.Items.INGOTS_IRON)
-            .define('c', Items.COPPER_INGOT)
-            .define('w', Items.WATER_BUCKET)
-            .define('l', Items.LAVA_BUCKET)
-            .define('r', Tags.Items.DUSTS_REDSTONE)
-            .pattern(" i ")
-            .pattern("wcl")
-            .pattern("rir")
-            .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-            .save(consumer);
-        
+                .shaped(RecipeCategory.MISC, TemperatureModule.TEMP_MODULE_BASIC)
+                .define('i', Tags.Items.INGOTS_IRON)
+                .define('c', Tags.Items.INGOTS_COPPER)
+                .define('w', Items.WATER_BUCKET)
+                .define('l', Items.LAVA_BUCKET)
+                .define('r', Tags.Items.DUSTS_REDSTONE)
+                .pattern(" i ")
+                .pattern("wcl")
+                .pattern("rir")
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
+                .save(consumer);
+
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.MISC, WaterModule.WATER_MODULE_BASIC)
-            .define('i', Tags.Items.INGOTS_IRON)
-            .define('c', Items.COPPER_INGOT)
-            .define('p', ItemTags.PLANKS)
-            .define('s', Items.BUCKET)
-            .pattern("isi")
-            .pattern("sps")
-            .pattern("csc")
-            .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-            .save(consumer);
-    
+                .shaped(RecipeCategory.MISC, WaterModule.WATER_MODULE_BASIC)
+                .define('i', Tags.Items.INGOTS_IRON)
+                .define('c', Tags.Items.INGOTS_COPPER)
+                .define('p', ItemTags.PLANKS)
+                .define('s', Items.BUCKET)
+                .pattern("isi")
+                .pattern("sps")
+                .pattern("csc")
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
+                .save(consumer);
+
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.MISC, WindModule.WIND_MODULE_BASIC)
-            .define('i', Tags.Items.STORAGE_BLOCKS_IRON)
-            .define('c', Items.COPPER_INGOT)
-            .define('d', ItemTags.WOODEN_PRESSURE_PLATES)
-            .define('b', Items.IRON_BARS)
-            .pattern("bdb")
-            .pattern("did")
-            .pattern("cdc")
-            .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-            .save(consumer);
-        
+                .shaped(RecipeCategory.MISC, WindModule.WIND_MODULE_BASIC)
+                .define('i', Tags.Items.STORAGE_BLOCKS_IRON)
+                .define('c', Tags.Items.INGOTS_COPPER)
+                .define('d', ItemTags.WOODEN_PRESSURE_PLATES)
+                .define('b', Items.IRON_BARS)
+                .pattern("bdb")
+                .pattern("did")
+                .pattern("cdc")
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
+                .save(consumer);
+
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.MISC, EcoUpgrade.ECO_UPGRADE)
-            .define('l', ItemTags.LEAVES)
-            .define('c', Items.COPPER_INGOT)
-            .define('p', ItemTags.PLANKS)
-            .define('s', Items.STICK)
-            .pattern("lcl")
-            .pattern("lll")
-            .pattern("sps")
-            .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-            .save(consumer);
-    
+                .shaped(RecipeCategory.MISC, EcoUpgrade.ECO_UPGRADE)
+                .define('l', ItemTags.LEAVES)
+                .define('c', Tags.Items.INGOTS_COPPER)
+                .define('p', ItemTags.PLANKS)
+                .define('s', Items.STICK)
+                .pattern("lcl")
+                .pattern("lll")
+                .pattern("sps")
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
+                .save(consumer);
+
         ShapedRecipeBuilder
-            .shaped(RecipeCategory.MISC, EcoUpgrade.ECO_PLUS_UPGRADE)
-            .define('l', ItemTags.LEAVES)
-            .define('c', Items.COPPER_INGOT)
-            .define('p', ItemTags.PLANKS)
-            .define('s', EcoUpgrade.ECO_UPGRADE)
-            .pattern("lcl")
-            .pattern("lll")
-            .pattern("sps")
-            .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-            .save(consumer);
+                .shaped(RecipeCategory.MISC, EcoUpgrade.ECO_PLUS_UPGRADE)
+                .define('l', ItemTags.LEAVES)
+                .define('c', Tags.Items.INGOTS_COPPER)
+                .define('p', ItemTags.PLANKS)
+                .define('s', EcoUpgrade.ECO_UPGRADE)
+                .pattern("lcl")
+                .pattern("lll")
+                .pattern("sps")
+                .unlockedBy("has_eco_upgrade", has(EcoUpgrade.ECO_UPGRADE))
+                .save(consumer);
+
+        ShapedRecipeBuilder
+                .shaped(RecipeCategory.MISC, EfficiencyUpgrade.EFFICIENCY_UPGRADE_BASIC)
+                .define('l', Tags.Items.STORAGE_BLOCKS_REDSTONE)
+                .define('c', Tags.Items.INGOTS_COPPER)
+                .define('p', ItemTags.PLANKS)
+                .define('s', EcoUpgrade.ECO_UPGRADE)
+                .pattern("lcl")
+                .pattern("lll")
+                .pattern("sps")
+                .unlockedBy("has_carrier", has(BlockCarrier.INSTANCE_ITEM))
+                .save(consumer);
     }
 }
